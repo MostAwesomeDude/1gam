8ecfef9bc4f8acf402d1c0f081a694ca545719aa
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-01-07 11:25:34 -0800

    Start building Pong.

---
+++ Pong.hs
@@ -0,0 +1,151 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Main where
+
+import Control.Lens
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State
+import Data.Array
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Word
+
+import Codec.Image.STB
+import Data.Bitmap.OpenGL
+import Graphics.Rendering.OpenGL
+import Graphics.UI.SDL as SDL
+
+import Gemstone.Box
+import Gemstone.Color
+import Gemstone.GL
+import Gemstone.Sprite
+
+data Velocity v = Velocity { _vX, _vY :: v }
+    deriving (Show)
+
+makeLenses ''Velocity
+
+data Animation v = Animation { _aSprite   :: Sprite v
+                             , _aVelocity :: Velocity v }
+    deriving (Show)
+
+makeLenses ''Animation
+
+data Timers = Timers { _tTimestamp :: Word32
+                     , _tDelta     :: Word32
+                     , _tFps       :: Float }
+    deriving (Show)
+
+makeLenses ''Timers
+
+data Globals = Globals { _gScreen    :: Surface
+                       , _gCharacter :: Animation GLfloat
+                       , _gQuitFlag  :: Bool
+                       , _gTimers    :: Timers }
+    deriving (Show)
+
+makeLenses ''Globals
+
+type Loop = StateT Globals IO ()
+
+resizeScreen :: GLsizei -> GLsizei -> IO Surface
+resizeScreen w h = let
+    flags = [OpenGL, DoubleBuf, Resizable]
+    in do
+        screen <- setVideoMode (fromIntegral w) (fromIntegral h) 32 flags
+        resizeViewport w h
+        return screen
+
+makeVelocity :: Num v => Velocity v
+makeVelocity = Velocity 0 0
+
+makeAnimation :: Num v => Sprite v -> Animation v
+makeAnimation s = Animation s makeVelocity
+
+makeTimers :: Timers
+makeTimers = Timers 0 0 0
+
+getInitialState :: IO Globals
+getInitialState = let
+    box = makeBox (Vertex2 (-0.9) (-0.9)) (Vertex2 0.9 0.9)
+    in do
+    screen <- resizeScreen 1 1
+    let anim = makeAnimation $ Colored blue box
+    return $ Globals screen anim False makeTimers
+
+coordsAt :: Int -> Int -> Int -> Int -> Int -> (Int, Int)
+coordsAt w _ dw dh i = let
+    w' = w `div` dw
+    (y, x) = i `divMod` w'
+    in (x * dw, y * dh)
+
+mma :: Fractional a => a -> a -> a
+mma new old = (19 * old + new) / 20
+
+updateTimestamp :: Word32 -> Timers -> Timers
+updateTimestamp w t = let
+    delta = w - (t ^. tTimestamp)
+    fps = 1000 / fromIntegral delta
+    in tTimestamp .~ w $ tDelta .~ delta $ tFps %~ mma fps $ t
+
+handleEvent :: Event -> Globals -> Globals
+handleEvent (KeyDown (Keysym SDLK_ESCAPE _ _)) = gQuitFlag .~ True
+handleEvent _ = id
+
+handleEvents :: Loop
+handleEvents = do
+    event <- lift pollEvent
+    modify $ handleEvent event
+    case event of
+        NoEvent -> return ()
+        VideoResize w h ->
+            gScreen <~ lift (resizeScreen (fromIntegral w) (fromIntegral h))
+        KeyDown (Keysym SDLK_DOWN _ _) ->
+            gCharacter . aSprite . sBox . bTag . bY -= 0.1
+        KeyDown (Keysym SDLK_UP _ _) ->
+            gCharacter . aSprite . sBox . bTag . bY += 0.1
+        KeyDown (Keysym SDLK_LEFT _ _) ->
+            gCharacter . aSprite . sBox . bTag . bX -= 0.1
+        KeyDown (Keysym SDLK_RIGHT _ _) ->
+            gCharacter . aSprite . sBox . bTag . bX += 0.1
+        _ -> lift . putStrLn $ show event
+    -- Continue until all events have been handled.
+    when (event /= NoEvent) handleEvents
+
+gravitate :: Loop
+gravitate = do
+    delta <- use $ gTimers . tDelta
+    let dT = realToFrac delta / 1000
+    -- Integrate acceleration to get velocity.
+    gCharacter . aVelocity . vY -= 9.8 * dT
+    y <- use $ gCharacter . aVelocity . vY
+    -- Integrate velocity to get position.
+    gCharacter . aSprite . sBox . bTag . bY += y * dT
+
+mainLoop :: Loop
+mainLoop = loop
+    where
+    box = Colored white $ makeBox (Vertex2 (-0.9) (-0.9)) (Vertex2 0.9 0.9)
+    loop = do
+        ticks <- lift getTicks
+        gTimers %= updateTimestamp ticks
+        fps <- use $ gTimers . tFps
+        delta <- use $ gTimers . tDelta
+        lift . putStrLn $ "Ticks: " ++ show delta ++ " (FPS: " ++ show (floor fps) ++ ")"
+        handleEvents
+        gravitate
+        lift clearScreen
+        lift . drawSprite $ box
+        lift finishFrame
+        q <- use gQuitFlag
+        unless q loop
+
+actualMain :: IO ()
+actualMain = do
+    initial <- getInitialState
+    checkExtensions
+    _ <- runStateT mainLoop initial
+    return ()
+
+main :: IO ()
+main = withInit [InitEverything] actualMain
