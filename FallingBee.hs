17c558494c37f8294d29efd01e8cac7f40ec904d
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-02-10 17:12:04 -0800

    Put a block at the top...

--- FallingBee.hs
+++ FallingBee.hs
@@ -2,38 +2,31 @@
 module Main where
 
 import Control.Lens
-import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
-import Data.Array
 import qualified Data.Map as M
-import Data.Maybe
 
-import Codec.Image.STB
-import Data.Bitmap.OpenGL
 import Graphics.Rendering.OpenGL
 import Graphics.UI.SDL as SDL
-import Linear as L
 
-import Gemstone.Animation
 import Gemstone.Box
 import Gemstone.Color
 import Gemstone.GL
 import Gemstone.Loop
 import Gemstone.Main
 import Gemstone.Sprite
-import Gemstone.Timers
 
 type Coords = (Int, Int)
 
 data Globals = Globals { _gTableau :: M.Map Coords Material
-                       , _gCurrentCoords :: (Int, Int) }
+                       , _gCurrentCoords :: (Int, Int)
+                       , _gCurrentMat :: Material }
     deriving (Show)
 
 makeLenses ''Globals
 
 getInitialState :: IO Globals
-getInitialState = return $ Globals M.empty (0, 0)
+getInitialState = return $ Globals M.empty (5, 9) (Colored red Nothing)
 
 matMap :: (Coords, Material) -> Sprite GLfloat
 matMap ((x, y), mat) = Sprite mat $ makeXYWHValid x' y' 0.1 0.1
@@ -55,9 +48,13 @@ mainLoop = gemstoneLoop pre draw (return ())
     draw :: Loop Globals
     draw = do
         lift clearScreen
-        zoom (_2 . gTableau) $ do
-            sprites <- gets (map matMap . M.toList)
-            lift $ drawSprites sprites
+        zoom _2 $ do
+            zoom gTableau $ do
+                sprites <- gets (map matMap . M.toList)
+                lift $ drawSprites sprites
+            coords <- use gCurrentCoords
+            mat <- use gCurrentMat
+            lift . drawSprite $ matMap (coords, mat)
         lift finishFrame
 
 main :: IO ()
